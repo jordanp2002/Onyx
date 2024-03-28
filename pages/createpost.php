bacb95db2fb8aba756c6a630604d253a5346e90d
jordanp2002 <jordanp2002@live.com>
2024-03-27 21:07:49 -0700

    changes

--- pages/createpost.php
+++ pages/createpost.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <title>Create Post</title>
+    <link rel="stylesheet" href="../css/home.css">
     <link rel="stylesheet" href="../css/createpost.css">
 </head>
 <?php
@@ -67,9 +68,9 @@
     <div class="PostCreation">
         <form class ="titlePost" action = "../pages/postcreationhandle.php" method = "POST">
             <label for="postTitle" type="hidden"></label>
-            <textarea id="postTitle" name="postTitle" rows="2" cols="50" maxlength="50" minlength="2" placeholder="Title here"></textarea>
+            <textarea id="postTitle" name="postTitle" rows="2" cols="50"  placeholder="Title here"></textarea>
             <label for="postContent" type="hidden"></label>
-            <textarea id="postContent" name="postContent" rows="4" cols="50" maxlength="500" minlength="10" placeholder="Text here max 500 characters"></textarea>
+            <textarea id="postContent" name="postContent" rows="4" cols="50" placeholder="Text here max 500 characters"></textarea>
             <label for="postCom" type = "hidden"></label>
             <input type ="text" id ="postCom" name = "postCom" placeholder = "Community">
             <br>
@@ -77,5 +78,25 @@
             <button class ="delete-button"type="button">Delete</button>
         </form>
     </div>
+    <script>
+        function validateForm(event) {
+            var titleInput = document.getElementById('postTitle');
+            var postInput = document.getElementById('postContent');
+
+            var title = titleInput.value.trim();
+            if (title.length < 2 || title.length > 50) {
+                alert('Post title must be between 2 and 50 characters.');
+                event.preventDefault();
+                return;
+            }
+            var content = postInput.value.trim();
+            if (content.length < 10 || content.length > 500) {
+                alert('Post content must be between 10 and 500 characters.');
+                event.preventDefault();
+                return;
+            }
+        }
+        document.querySelector('.titlePost').addEventListener('submit', validateForm);
+    </script>
 </body>
 </html>
--- pages/databaseconnection.php
+++ pages/databaseconnection.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_USERNAME', '76966621');
+define('DB_PASSWORD', 'Password123');
 define('DB_NAME', 'db_76966621');
 ?>
